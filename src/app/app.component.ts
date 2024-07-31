b978cccda6532817aef5731ce290fb5f2114b04e
Reucov David <david.reucov@topin.hu>
2024-07-31 09:36:03 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -13,9 +13,25 @@ export class AppComponent implements AfterViewInit {
   bottomUIHeight: number | undefined;
 
   ngAfterViewInit(): void {
+    // Initial calculation with a slight delay
+    setTimeout(() => {
+      this.calculateBottomUIHeight();
+    }, 100); // Adjust delay as needed
+  }
+
+  @HostListener('window:resize')
+  @HostListener('window:scroll')
+  onResizeOrScroll() {
+    this.calculateBottomUIHeight();
+  }
+
+  calculateBottomUIHeight() {
     const layoutViewportHeight = window.innerHeight;
     const visualViewportHeight = window.visualViewport ? window.visualViewport.height : layoutViewportHeight;
 
     this.bottomUIHeight = layoutViewportHeight - visualViewportHeight;
+    alert('Layout Viewport Height:' + layoutViewportHeight);
+    alert('Visual Viewport Height:' + visualViewportHeight);
+    alert('Calculated Bottom UI Height: ' + this.bottomUIHeight);
   }
 }
