6042928e70373001cab703a4bb99589b94156e85
Reucov David <reucovd@gmail.com>
2024-12-10 21:43:47 +0100

    added hu translation route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HuComponent } from './hu/hu.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'hu',
+    component: HuComponent,
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
